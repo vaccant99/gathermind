a10703460c5602b9a31d872b93d718aa5c50ae40
Limselect <seontaek1998@gmail.com>
2024-11-07 22:46:09 +0900

    241107 02: fixed bug

--- src/main/java/woongjin/gatherMind/controller/HomeController.java
+++ src/main/java/woongjin/gatherMind/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = {"/", "/login", "/signup", "/mypage", "/mypage/*", "/editprofile", "/goodbye"})
+    @RequestMapping(value = {"/", "/login", "/signup", "/mypage", "/mypage/*", "/editprofile", "/goodbye", "/serious"})
     public String index() {
         return "forward:/index.html";
     }
