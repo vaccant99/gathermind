70222c3198dfb5952bca23388eaec6dc404ca909
Vacant <vaccant99@gmail.com>
2024-11-14 16:52:03 +0900

    main back

--- src/main/java/woongjin/gatherMind/controller/AuthController.java
+++ src/main/java/woongjin/gatherMind/controller/AuthController.java
@@ -1,13 +1,6 @@
 package woongjin.gatherMind.controller;
 
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
