31bdfc36548cb2d599a3325e66b1d0ebb604e27a
Limselect <seontaek1998@gmail.com>
2024-11-25 09:18:49 +0900

    change build

--- src/main/java/woongjin/gatherMind/config/LoggingConfig.java
+++ src/main/java/woongjin/gatherMind/config/LoggingConfig.java
@@ -1,20 +1,20 @@
-package woongjin.gatherMind.config;
-
-import jakarta.servlet.Filter;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.AbstractRequestLoggingFilter;
-import org.springframework.web.util.ContentCachingRequestWrapper;
-
-import java.nio.charset.StandardCharsets;
-
-
-@Configuration
-public class LoggingConfig {
-
-    @Bean
-    public Filter cachingFilter() {
-        return new CachingFilter();
-    }
-}
+//package woongjin.gatherMind.config;
+//
+//import jakarta.servlet.Filter;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.filter.AbstractRequestLoggingFilter;
+//import org.springframework.web.util.ContentCachingRequestWrapper;
+//
+//import java.nio.charset.StandardCharsets;
+//
+//
+//@Configuration
+//public class LoggingConfig {
+//
+//    @Bean
+//    public Filter cachingFilter() {
+//        return new CachingFilter();
+//    }
+//}
